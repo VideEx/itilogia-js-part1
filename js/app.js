2533c6c25acece4760e14fab79789f384524f691
VideEx <zlakomanovak@gmail.com>
2024-07-11 14:55:41 +0300

    First version of the project

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
-import {Chart} from "./chart.min.js";
+// import {Chart} from "./chart.min.js";
 
 let incomeData = [
     {
         value: 20,
-        color:"#878BB6"
+        color: "rgba(220, 53, 69, 1)"
     },
     {
-        value : 40,
-        color : "#4ACAB4"
+        value: 30,
+        color: "rgba(32, 201, 151, 1)"
     },
     {
-        value : 10,
-        color : "#FF8153"
+        value: 10,
+        color: "rgba(13, 110, 253, 1)"
     },
     {
-        value : 30,
-        color : "#FFEA88"
+        value: 30,
+        color: "rgba(255, 193, 7, 1)"
+    },
+    {
+        value: 30,
+        color: "rgba(253, 126, 20, 1)"
     }
 ];
 
 let incomeOptions = {
-    segmentShowStroke : false,
-    animateScale : true
+    segmentShowStroke: false,
+    animateScale: true,
+    legend: {
+        display: true,
+        position: 'top',
+        labels: {
+            fontColor: 'black',
+            fontFamily: 'Calibri Light',
+            fontStyle: 'italic'
+        }
+    }
 }
 
 let income = document.getElementById("income").getContext("2d");
@@ -31,25 +44,49 @@ new Chart(income).Pie(incomeData, incomeOptions);
 let expensesData = [
     {
         value: 20,
-        color:"#878BB6"
+        color: "rgba(220, 53, 69, 1)"
     },
     {
-        value : 40,
-        color : "#4ACAB4"
+        value: 30,
+        color: "rgba(32, 201, 151, 1)"
     },
     {
-        value : 10,
-        color : "#FF8153"
+        value: 10,
+        color: "rgba(13, 110, 253, 1)"
     },
     {
-        value : 30,
-        color : "#FFEA88"
+        value: 30,
+        color: "rgba(255, 193, 7, 1)"
+    },
+    {
+        value: 30,
+        color: "rgba(253, 126, 20, 1)"
     }
 ];
 
 let expensesOptions = {
-    segmentShowStroke : false,
-    animateScale : true
+    segmentShowStroke: false,
+    animateScale: true,
+    options: {
+        aspectRatio: 2,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'left',
+                reverse: true, // Reverse order of legend labels
+                title: {
+                    display: true,
+                    text: 'Number of Days',
+                    font: {
+                        size: 14,
+                        weight: 'bold'
+                    },
+                    padding: 20,
+                    color: 'green'
+                }
+            }
+        }
+    }
 }
 
 let expenses = document.getElementById("expenses").getContext("2d");
